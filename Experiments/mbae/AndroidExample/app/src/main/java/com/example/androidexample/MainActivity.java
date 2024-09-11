2d08b216a5aaadb26b4a4b0acf8f1238f2f3caa3
mbae <mbae@iastate.edu>
2024-09-11 14:28:35 -0500

    updated

--- Experiments/mbae/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiments/mbae/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -2,13 +2,8 @@ package com.example.androidexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,8 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);             // link to Main activity XML
 
         /* initialize UI elements */
-        messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
+        messageText = findViewById(R.id.h);      // link to message textview in the Main activity XML
         messageText.setText("Hello World");
-        messageText.setText("This is my hello world for demo1");
+
     }
 }
