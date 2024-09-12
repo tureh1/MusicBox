e72ad6fcaad5fd71c8d8cdf8ced315cc77ca38ee
Tu <tureh@iastate.edu>
2024-09-12 17:08:29 -0500

    modification to helloworld file

--- Experiments/tureh/tutorials-springboot_unit1_1_helloworld/helloworld/src/main/java/coms309/WelcomeController.java
+++ Experiments/tureh/tutorials-springboot_unit1_1_helloworld/helloworld/src/main/java/coms309/WelcomeController.java
@@ -3,17 +3,33 @@ package coms309;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import java.time.LocalDateTime;
+import java.util.Random;
 
 @RestController
 class WelcomeController {
 
+    //An Array of quotes to be randomly selected
+    private String[] quotes = {
+            "Believe in yourself.",
+            "Persevere and don't give up.",
+            "You are stronger than you think."
+    };
+
     @GetMapping("/")
     public String welcome() {
-        return "Hello and welcome to COMS 309";
+        return formatMessage("Hello and welcome to COMS 309");
     }
 
+    //@PathVariable String name method runs a format with name included
     @GetMapping("/{name}")
     public String welcome(@PathVariable String name) {
-        return "Hello and welcome to COMS 309: " + name;
+        return formatMessage("Hello " + name + " and welcome to COMS 309");
+    }
+
+    private String formatMessage(String message) {
+        String timestamp = LocalDateTime.now().toString(); //Get time
+        String quote = quotes[new Random().nextInt(quotes.length)]; //Select random quotes
+        return message + "\nCurrent time: " + timestamp + "\nQuote: " + quote;
     }
 }
