fc5771c16918c12df8e60545a24d72d16b911418
Ping <pgwu@iastate.edu>
2024-10-11 13:18:11 -0500

    update

--- Frontend/MusiBox/app/src/main/java/com/example/musibox/ForgotActivity.java
+++ Frontend/MusiBox/app/src/main/java/com/example/musibox/ForgotActivity.java
@@ -63,7 +63,6 @@ public class ForgotActivity extends AppCompatActivity {
 
         return true;
     }
-
     private void sendPasswordRequest(String email, String password) {
         String url = "http://10.90.72.167:8080/newpass/" + email;
 
