397fcf75baa4a1066a3c807c278e729bf5e727ae
Tu <tureh@iastate.edu>
2024-11-05 19:29:50 -0600

    code didnt update

--- Backend/backend/springboot_example/src/main/java/onetomany/GroupPlaylist/GroupPlaylistRepository.java
+++
@@ -1,6 +0,0 @@
-package onetomany.GroupPlaylist;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupPlaylistRepository extends JpaRepository<GroupPlaylist, Long> {
-}
